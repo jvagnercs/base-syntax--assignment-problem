b378824d8cac637446e8084f90f05426f29ae093
jvagnercs <jvagnercs@gmail.com>
2018-11-19 09:22:59 -0200

    p change on UserOutput.js

--- src/Components/UserOutput.js
+++ src/Components/UserOutput.js
@@ -3,7 +3,7 @@ import React from 'react'
 const userOutput = props => (
     <div>
         <p style={props.style}>1st paragraph</p>
-        <p>2nd paragraph:<br/> {props.children}</p>
+        <p>User name:<br/> {props.children}</p>
     </div>
 )
 
